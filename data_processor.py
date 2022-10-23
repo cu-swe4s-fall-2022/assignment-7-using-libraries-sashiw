08d72a36b8696e434235029b7d805894f9554f54
sashiw <sashi.weerawarana@colorado.edu>
2022-10-23 17:11:25 -0600

    has all required functions

--- data_processor.py
+++ data_processor.py
@@ -1,10 +1,69 @@
-# remember to import your libraries!
+import numpy as np
+import pandas as pd
 
 def get_random_matrix(num_rows, num_columns):
-	return None
+    '''generate a random matrix of floats
+
+    Parameters
+    ----------
+    num_rows: number of rows
+              integer greater than 0
+
+    num_rows: number of columns
+              integer greater than 0
+
+    Returns
+    -------
+    matrix
+        2 dimensional numpy array
+
+    '''
+    np.random.seed(2)
+    matrix = np.random.rand(num_rows, num_columns)
+    
+    return matrix
 
 def get_file_dimensions(file_name):
-	return (0,0)
+    '''return the dimensions of tabular csv
+    
+    Parameters
+    ----------
+    file_name: file name
+               csv file name as string
+    
+    Returns
+    -------
+    dimensions
+        tupule with .csv dimensions
+    
+    '''
+    data = pd.read_csv(file_name, sep = ',', header=None)
+    dims = data.shape
+    
+    return (dims)
+
 
 def write_matrix_to_file(num_rows, num_columns, file_name):
-	return None
+    '''create a .csv file with a matrix of floats
+
+    Parameters
+    ----------
+    num_rows: number of rows
+              integer greater than 0
+
+    num_rows: number of columns
+              integer greater than 0
+
+    file_name: file_name.csv
+               .csv file to write matrix in
+
+    Returns
+    -------
+    csv_file
+        csv file with matrix
+    
+    '''
+    matrix = get_random_matrix(num_rows, num_columns)
+    csv_file = np.savetxt(file_name, matrix, delimiter=',')
+
+    return csv_file
