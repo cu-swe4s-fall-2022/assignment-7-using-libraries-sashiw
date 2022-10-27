880176782a2b8a5a55dac4c56d5e0f13d3c76597
sashiw <sashi.weerawarana@colorado.edu>
2022-10-27 10:30:10 -0600

    functional tests

---
+++ functional_tests.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+test -e ssshtest || wget -qhttps://raw.githubusercontent.com/ryanlayer/ssshtest/master/ssshtest
+. ssshtest
+
+# test get_random_matrix
+run get_random_matrix python func_tests_script.py --test_type random_matrix
+    assert_exit_code 0
+
+# test get_file_dimensions
+run get_random_matrix python func_tests_script.py --test_type file_dims
+    assert_exit_code 0
+
+# test write_matrix_to_file
+run get_random_matrix python func_tests_script.py --test_type write_matrix
+    assert_exit_code 0
+
+# test plotter
+run plotter python plotter.py
+    assert_exit_code 0
